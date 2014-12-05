27ede565a7a9c4bc7074e3f5112e3b3780d29e84
JulioLopez <juliojlgon@gmail.com>
2014-12-05 15:11:53 +0100

    Splash Layout

--- app/src/main/java/se/ESNBTH/esnbth/Splash_Screen.java
+++ app/src/main/java/se/ESNBTH/esnbth/Splash_Screen.java
@@ -1,18 +1,68 @@
 package se.ESNBTH.esnbth;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 import se.ESNBTH.esnbth.R;
 
 public class Splash_Screen extends Activity {
 
+    private static int SPLASH_TIMEOUT = 1500;
+    Animation fadeOut;
+    RelativeLayout splashLay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        splashLay = (RelativeLayout) findViewById(R.id.splashLayout);
+
+        fadeOut = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_out);
+
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                //nothing
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent i = new Intent(Splash_Screen.this, MainLayActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                //Nothing
+            }
+        });
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+
+                splashLay.startAnimation(fadeOut);
+
+            }
+        }, SPLASH_TIMEOUT);
     }
 
 
